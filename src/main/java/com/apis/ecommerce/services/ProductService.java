641ccdac1b6aec93485415d4fafd807a70c52586
DarVerZi <69283977+DarVerZi@users.noreply.github.com>
2024-07-23 14:24:13 -0300

    agregamos paginado en producto

--- src/main/java/com/apis/ecommerce/services/ProductService.java
+++ src/main/java/com/apis/ecommerce/services/ProductService.java
@@ -3,6 +3,8 @@ package com.apis.ecommerce.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.apis.ecommerce.entities.Product;
@@ -13,7 +15,7 @@ import com.apis.ecommerce.exceptions.ProductNonexistentException;
 
 @Service
 public interface ProductService {
-    public List<Product> getProduct();
+    public Page<Product> getProduct(PageRequest pageRequest);
     public Optional<Product> getProductById(Long id);
     public List<Product> getProductsByCategoryId(Long id);  
     public Product createProduct(ProductRequest p) throws ProductDuplicateException;
