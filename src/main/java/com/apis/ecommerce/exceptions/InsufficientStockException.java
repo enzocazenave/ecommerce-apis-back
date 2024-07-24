f764bcaa94acabc6e8d1284a27090a99e7d83dac
Yamil Rios <gabriel.rios.93@gmail.com>
2024-07-24 00:21:06 -0300

    Fix Stock exception

--- src/main/java/com/apis/ecommerce/exceptions/InsufficientStockException.java
+++ src/main/java/com/apis/ecommerce/exceptions/InsufficientStockException.java
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "El producto no posee stock suficiente.")
-public class InsufficientStockException extends Throwable {
+public class InsufficientStockException extends RuntimeException {
 }
